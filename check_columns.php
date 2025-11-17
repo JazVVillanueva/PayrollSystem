4fed73c7b6ac9beb2c84d75cce012b86ab7c12dd
Jaz <jaz_villanueva@dlsu.edu.ph>
2025-11-17 22:23:44 +0800

    Remove debug and helper files

--- check_columns.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'db_connect.php';
-$r = $conn->query('SHOW COLUMNS FROM timesheet');
-echo "Timesheet Columns:\n";
-while($row = $r->fetch_assoc()) {
-    echo $row['Field'] . "\n";
-}
-?>
